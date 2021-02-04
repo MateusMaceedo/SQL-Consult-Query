1514ab1c0a25c2b87864101ead23b9b3d0ce024b
Mateus Macedo <63172367+MateusMaceedo@users.noreply.github.com>
2021-02-03 22:06:33 -0300

    Add files via upload

---
+++ ep01/hello.sql
@@ -0,0 +1,16 @@
+-- selecionado campos da TABLE
+/*
+select seller_id,
+seller_city
+from tb_sellers
+*/
+
+-- selecionando campos da TABLE, com limites
+/*
+select *
+FROM tb_sellers
+LIMIT 1
+*/
+
+select *
+from tb_products
