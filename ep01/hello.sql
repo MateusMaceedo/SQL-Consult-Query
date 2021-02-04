48e3b1db31e76679603c5db085cadb1423928ac9
Mateus Macedo <63172367+MateusMaceedo@users.noreply.github.com>
2021-02-03 22:51:22 -0300

    Add files via upload

--- ep01/hello.sql
+++ ep01/hello.sql
@@ -1,16 +1,6 @@
--- selecionado campos da TABLE
-/*
-select seller_id,
-seller_city
-from tb_sellers
-*/
-
--- selecionando campos da TABLE, com limites
-/*
-select *
-FROM tb_sellers
-LIMIT 1
-*/
-
-select *
+select product_id,
+       product_category_name,
+       product_photos_qty
 from tb_products
+
+where product_category_name = 'bebes'
